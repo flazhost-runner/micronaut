04d7ebad006e60b20f55bcc6f3cd89ef02cb2742
Sergio del Amo <sergio.delamo@softamo.com>
2025-01-21 09:11:19 +0100

    add default test

--- http-client/src/test/java/io/micronaut/http/client/HostnameVerificationTest.java
+++ http-client/src/test/java/io/micronaut/http/client/HostnameVerificationTest.java
@@ -1,29 +1,31 @@
 package io.micronaut.http.client;
-import io.micronaut.context.BeanContext;
+
 import io.micronaut.context.annotation.Property;
+import io.micronaut.core.util.StringUtils;
 import io.micronaut.http.ssl.ClientSslConfiguration;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest(startApplication = false)
 class HostnameVerificationTest {
-    @Inject
-    BeanContext beanContext;
     @Test
-    @Property(name = "micronaut.http.client.ssl.disable-hostname-verification", value = "true")
-    void testSslConfigurationWithHostnameVerificationDisabled() {
-        ClientSslConfiguration sslConfig = beanContext.getBean(ClientSslConfiguration.class);
+    @Property(name = "micronaut.http.client.ssl.disable-hostname-verification", value = StringUtils.TRUE)
+    void testSslConfigurationWithHostnameVerificationDisabled(ClientSslConfiguration sslConfig) {
         assertTrue(sslConfig.isDisableHostnameVerification(),
             "Expected hostname verification to be disabled");
     }
 
     @Test
-    @Property(name = "micronaut.http.client.ssl.disable-hostname-verification", value = "false")
-    void testSslConfigurationWithHostnameVerificationEnabled() {
-        ClientSslConfiguration sslConfig = beanContext.getBean(ClientSslConfiguration.class);
+    void testSslConfigurationWithHostnameVerificationDisabledByDefault(ClientSslConfiguration sslConfig) {
+        assertFalse(sslConfig.isDisableHostnameVerification(),
+            "Expected hostname verification to be enabled");
+    }
+
+    @Test
+    @Property(name = "micronaut.http.client.ssl.disable-hostname-verification", value = StringUtils.FALSE)
+    void testSslConfigurationWithHostnameVerificationEnabled(ClientSslConfiguration sslConfig) {
         assertFalse(sslConfig.isDisableHostnameVerification(),
             "Expected hostname verification to be enabled");
     }
