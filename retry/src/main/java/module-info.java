2426d911b22f1e28ce4c100abff67457130b80a2
Graeme Rocher <graeme.rocher@oracle.com>
2026-02-27 09:28:18 -0400

    chore: ensure JPMS descriptors conform to Spotless/Checkstyle

--- retry/src/main/java/module-info.java
+++ retry/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2025 original authors
+ * Copyright 2017-2026 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+/**
+ * JPMS module descriptor.
  */
 module io.micronaut.retry {
     requires transitive io.micronaut.context;
