046eb728845afaac71dcfb71a20114bdd290d5ee
yawkat <jonas.konrad@oracle.com>
2026-03-20 09:38:48 +0000

    Drop obsolete GraalVM reproducer change
    Co-Authored-By: multicode <multicode@yawk.at>

--- test-suite-kotlin-graalvm/src/test/kotlin/example/micronaut/HelloController.kt
+++ test-suite-kotlin-graalvm/src/test/kotlin/example/micronaut/HelloController.kt
@@ -5,9 +5,7 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.toList
 
 @Controller("/hello")
 class HelloController {
@@ -19,12 +17,7 @@ class HelloController {
     }
 
     @Get(value = "/world2", produces = [MediaType.TEXT_PLAIN])
-    suspend fun world2(): Flow<String> {
-        val values = callDelayed().toList()
-        return values.asFlow()
-    }
-
-    private fun callDelayed(): Flow<String> = flow {
+    suspend fun world2(): Flow<String> = flow {
         delay(1000)
         listOf("Hello World").forEach { emit(it) }
     }
