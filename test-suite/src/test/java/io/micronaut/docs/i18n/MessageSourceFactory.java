c6ca8782de7338732e887d090f38c9e941bcb284
Sergio del Amo <sergio.delamo@softamo.com>
2026-04-28 07:50:57 +0200

    Merge commit from fork

--- test-suite/src/test/java/io/micronaut/docs/i18n/MessageSourceFactory.java
+++ test-suite/src/test/java/io/micronaut/docs/i18n/MessageSourceFactory.java
@@ -1,11 +1,15 @@
 package io.micronaut.docs.i18n;
 
-//tag::clazz[]
+//tag::imports[]
 import io.micronaut.context.MessageSource;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.i18n.ResourceBundleMessageSource;
 import jakarta.inject.Singleton;
+//end::imports[]
 
+@Requires(property = "spec.name", value = "I18nSpec")
+//tag::clazz[]
 @Factory
 class MessageSourceFactory {
     @Singleton
